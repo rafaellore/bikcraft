0701aa3cc29c257e23cf8a123806e9a20895e952
Rafaellore <birdneon3@gmail.comxo>
2022-02-25 17:49:27 -0300

    adicionando animação e mais

---
+++ js/script.js
@@ -0,0 +1,63 @@
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// Ativar Items do Orçamento - Pega os parametros da url e joga eles num array
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//Galeria de bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+galeria.forEach((img) => {
+  img.addEventListener("click", () => {
+    if (matchMedia("(min-width: 1000px)").matches) {
+      galeriaContainer.prepend(img);
+    }
+  });
+});
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
